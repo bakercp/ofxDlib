0b73ece3c2594e286aa482b6f57957a91ca17c84
Christopher Baker <me@christopherbaker.net>
2017-03-19 14:49:39 -0500

    Update script.

--- scripts/download-models.sh
+++ scripts/download-models.sh
@@ -1,29 +1,82 @@
 #!/bin/sh
 
-# Download dlib models.
+# Download dlib models and media.
 
-base_url=http://dlib.net/files
+echo "Downloading models and media .."
 
-model_path=../models
+ADDON_PATH=$( cd $(dirname $0)/.. ; pwd -P )
+SCRIPTS_PATH=$ADDON_PATH/scripts
 
-models=(
-  "dlib_face_recognition_resnet_model_v1.dat.bz2"
-  "imagenet2015_validation_images.txt.bz2"
-  "mmod_dog_hipsterizer.dat.bz2"
-  "mmod_human_face_detector.dat.bz2"
-  "resnet34_1000_imagenet_classifier.dnn.bz2"
-  "shape_predictor_68_face_landmarks.dat.bz2"
+MODELS_PATH=$ADDON_PATH/models
+
+dlib_model_base_url="http://dlib.net/files"
+dlib_model_compressed_suffix=".bz2"
+dlib_models=(
+  "dlib_face_recognition_resnet_model_v1.dat"
+  "mmod_dog_hipsterizer.dat"
+  "mmod_human_face_detector.dat"
+  "resnet34_1000_imagenet_classifier.dnn"
+  "shape_predictor_68_face_landmarks.dat"
 )
 
-for model in "${models[@]}"
+for dlib_model in "${dlib_models[@]}"
 do
+  dlib_model_compressed=$dlib_model$dlib_model_compressed_suffix
+  dlib_model_compressed_path=$MODELS_PATH/$dlib_model_compressed
+  dlib_model_path=$MODELS_PATH/$dlib_model
 
-echo "Downloading $model"
-curl -L -o $model_path/$model --progress-bar $base_url/$model
+  echo "Downloading: $dlib_model"
 
-echo "Extracting $model"
-bzip2 -d $model_path/$model
+  if ! [ -f $dlib_model_path ] && ! [ -f $dlib_model_compressed_path ] ; then
+    curl -L -o $dlib_model_compressed_path --progress-bar $dlib_model_base_url/$dlib_model_compressed
+  else
+    echo "- Exists: Skipping download $model"
+  fi
 
+  if ! [ -f $dlib_model_path ] ; then
+    echo "Decompressing $dlib_model_compressed_path"
+    bzip2 -d $dlib_model_compressed_path
+  else
+    echo "- Exists: Skipping decompression $model"
+  fi
+
+  echo ""
 done
 
-echo "Done"
+echo ""
+echo "Installing example models ..."
+
+for required_models in `ls $ADDON_PATH/example*/bin/data/required_models.txt`
+do
+  while read model || [ -n "$model" ];
+  do
+    cp -v $MODELS_PATH/$model $(dirname $required_models)/
+  done < $required_models
+  echo ""
+done
+
+echo ""
+echo "Downloading example media ..."
+
+for required_media in `ls $ADDON_PATH/example*/bin/data/required_media.txt`
+do
+  # Move into this directory. > /dev/null consumes the output.
+  pushd $(dirname $required_media)/ > /dev/null
+
+  # The || [ -n "$line" ]; is to help when the last line isn't a new line char.
+  while read line || [ -n "$line" ];
+  do
+    tokens=($line)
+    url=${tokens[0]}
+    destination=${tokens[1]}
+    echo "Downloading $url ..."
+    if [ -z "$destination" ];
+    then
+      curl -L -O --progress-bar $url
+    else
+      curl -L -o $destination --progress-bar $url
+    fi
+  done < $required_media
+  popd
+  echo ""
+done
