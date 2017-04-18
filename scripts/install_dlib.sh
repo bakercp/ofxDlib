f3236b463d47b3ad5a211047b984893a381a884e
Christopher Baker <me@christopherbaker.net>
2017-04-18 17:30:12 -0500

    Add support for automatically installing apothecary to build scripts.

--- scripts/install_dlib.sh
+++ scripts/install_dlib.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 
+APOTHECARY_DIR=../../../scripts/apothecary/
+
+
+if ! [ -f $APOTHECARY_DIR/apothecary/apothecary ] ; then
+  echo "Apothecary not installed, getting."
+  git clone https://github.com/openframeworks/apothecary.git $APOTHECARY_DIR/
+else
+  echo "- Exists: Pulling"
+  pushd $APOTHECARY_DIR/apothecary > /dev/null
+  git pull
+  popd > /dev/null
+fi
+
+
+
 rm -rf  ../../../scripts/apothecary/apothecary/build/dlib/
 ../../../scripts/apothecary/apothecary/./apothecary -v -j16 -d ../libs update ofxDlib
 
