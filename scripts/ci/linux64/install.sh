61f6704718bd1e64251fbac1ace4aab85a1e0bcf
Christopher Baker <me@christopherbaker.net>
2017-04-19 06:57:03 -0500

    Continued fixes for CI.

--- scripts/ci/linux64/install.sh
+++ scripts/ci/linux64/install.sh
@@ -3,4 +3,12 @@
 echo "Installing ... "
 echo `pwd`
 
-/bin/bash install_dlib.sh
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}/../../" )" && pwd )"
+
+echo $SCRIPTS_DIR
+
+# Installing ...
+# /home/travis/build/bakercp/ofxDlib
+# /bin/bash: install_dlib.sh: No such file or directory
+
+/bin/bash $SCRIPTS_DIR/install_dlib.sh
