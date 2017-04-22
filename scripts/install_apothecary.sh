57ff97897b6327a27f28d27d3e11c0c372821809
Christopher Baker <me@christopherbaker.net>
2017-04-22 15:52:23 -0500

    Update apothecary install script.

---
+++ scripts/install_apothecary.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This script assumes it is in the openFrameworks/addons/THE_ADDON/scripts/ dir.
+OF_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../" && pwd )"
+
+# This is the default location for apothecary.
+APOTHECARY_DIR=$OF_ROOT/scripts/apothecary
+
+# Check to see if apothecary is already installed.
+if ! [ -f $APOTHECARY_DIR/apothecary/apothecary ] ; then
+  echo "Apothecary not installed, installing latest version."
+  git clone https://github.com/openframeworks/apothecary.git $APOTHECARY_DIR/
+else
+  pushd $APOTHECARY_DIR/ > /dev/null
+  if git rev-parse --is-inside-work-tree ; then
+      echo "Apothecary is under git control, updating."
+      git pull origin master
+  else
+      echo "Apothecary is not under git control, so it may not be up-to-date."
+  fi
+  popd > /dev/null
+fi
+
+# Move into the apothecary directory.
+pushd $APOTHECARY_DIR/apothecary/ > /dev/null
+# Determine target os.
+TARGET_OS=`./ostype.sh`
+# Install apthecary requirements.
+popd > /dev/null
+
+echo $APOTHECARY_DIR/apothecary/scripts/$TARGET_OS/install.sh
+
+# Install any dependencies.
+if [ -f $APOTHECARY_DIR/scripts/$TARGET_OS/install.sh ] ; then
+  $APOTHECARY_DIR/scripts/$TARGET_OS/install.sh
+fi
+
+echo "Done installing apothecary."
