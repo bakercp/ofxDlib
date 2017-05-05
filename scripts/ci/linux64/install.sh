31b7fabaa758003cc3ea9f6020cd43c9b8ba4c37
Christopher Baker <me@christopherbaker.net>
2017-05-04 22:58:45 -0500

    Update CI install scripts.

--- scripts/ci/linux64/install.sh
+++ scripts/ci/linux64/install.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-echo "Installing ... "
-echo `pwd`
+cd ../../
 
-# Make sure realpath is available
-sudo apt-get install -y coreutils
-
-SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../" && pwd )"
-
-echo $SCRIPTS_DIR
-
-# Installing ...
-# /home/travis/build/bakercp/ofxDlib
-# /bin/bash: install_dlib.sh: No such file or directory
-
-pushd $SCRIPTS_DIR/ >> /dev/null
-
-/bin/bash install_dlib.sh
-
-popd >> /dev/null
+/bin/bash build.sh
