881b916a975e9b96c5255526be45c621f38cbb14
Christopher Baker <me@christopherbaker.net>
2018-01-19 22:14:23 -0600

    Add install scripts.

---
+++ scripts/linux/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+
+sudo apt-get -qq install -y cmake libblas-dev liblapack-dev libpng-dev libcairo2-dev libjpeg-dev libgif-dev > /dev/null
---
+++ scripts/osx/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Nothing to do for osx."
