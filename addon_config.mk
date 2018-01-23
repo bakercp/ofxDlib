8c547b91257517dec6a7c38a3c5c75d8186b30d5
Christopher Baker <me@christopherbaker.net>
2018-01-23 16:35:33 -0600

    Default to MKL / CUDA on linux64.

--- addon_config.mk
+++ addon_config.mk
@@ -39,10 +39,10 @@ linux64:
 	ADDON_PKG_CONFIG_LIBRARIES+=blas lapack
 
 	# If dlib is compiled with CUDA support, you made need to include these.
-	# ADDON_LDFLAGS+=-L/usr/local/cuda/lib64 -lcuda -lcudart -lcudnn -lcublas -lcurand -lcusolver
+	ADDON_LDFLAGS+=-L/usr/local/cuda/lib64 -lcuda -lcudart -lcudnn -lcublas -lcurand -lcusolver
 
 	# If dlib is compiled with MKL support, you may need to add an include here.
-	# ADDON_INCLUDES+=/opt/intel/mkl/include
+	ADDON_INCLUDES+=/opt/intel/mkl/include
 
 android/armeabi-v7a:
 	ADDON_LIBS = libs/dlib/lib/android/armeabi-v7a/libdlib.a
