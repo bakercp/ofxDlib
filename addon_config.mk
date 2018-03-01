56e1b2bc7b05eab198cf4c908b55881489d1193e
Christopher Baker <me@christopherbaker.net>
2018-02-28 23:15:18 -0600

    Don't use MKL or CUDA by default.

--- addon_config.mk
+++ addon_config.mk
@@ -29,15 +29,14 @@ osx:
 	# If your processor supports SIMD AVX instructions.
 	ADDON_CPPFLAGS += -mavx
 
-	# If dlib is compiled with MKL support, you may need to add an include here.
-#	ADDON_INCLUDES += /opt/intel/mkl/include
-#	ADDON_INCLUDES += /opt/intel/include
-
-#	ADDON_LDFLAGS += /opt/intel/mkl/lib/libmkl_intel_ilp64.a
-#	ADDON_LDFLAGS += /opt/intel/mkl/lib/libmkl_sequential.a
-#	ADDON_LDFLAGS += /opt/intel/mkl/lib/libmkl_core.a
-#	ADDON_LDFLAGS += -lm -ldl
-#	ADDON_DEFINES += MKL_ILP64
+	# If dlib is compiled with MKL support, you need to add these.
+	# ADDON_INCLUDES += /opt/intel/mkl/include
+	# ADDON_INCLUDES += /opt/intel/include
+	# ADDON_LDFLAGS += /opt/intel/mkl/lib/libmkl_intel_ilp64.a
+	# ADDON_LDFLAGS += /opt/intel/mkl/lib/libmkl_sequential.a
+	# ADDON_LDFLAGS += /opt/intel/mkl/lib/libmkl_core.a
+	# ADDON_LDFLAGS += -lm -ldl
+	# ADDON_DEFINES += MKL_ILP64
 
 linux64:
 	ADDON_LIBS = libs/dlib/lib/linux64/libdlib.a
@@ -50,11 +49,11 @@ linux64:
 	# If dlib is compiled with libblas/liblapack support, you may need to include these.
 	ADDON_PKG_CONFIG_LIBRARIES += blas lapack
 
-	# If dlib is compiled with CUDA support, you made need to include these.
-	ADDON_LDFLAGS += -L/usr/local/cuda/lib64 -lcuda -lcudart -lcudnn -lcublas -lcurand -lcusolver
+	# If dlib is compiled with CUDA support, you need to include these.
+	# ADDON_LDFLAGS += -L/usr/local/cuda/lib64 -lcuda -lcudart -lcudnn -lcublas -lcurand -lcusolver
 
-	# If dlib is compiled with MKL support, you may need to add an include here.
-	ADDON_INCLUDES += /opt/intel/mkl/include
+	# If dlib is compiled with MKL support, you need to include these.
+	# ADDON_INCLUDES += /opt/intel/mkl/include
 
 android/armeabi-v7a:
 	ADDON_LIBS = libs/dlib/lib/android/armeabi-v7a/libdlib.a
