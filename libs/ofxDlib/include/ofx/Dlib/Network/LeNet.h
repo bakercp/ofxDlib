dbed8498981c8871d41be2c6bbe7783491499bb9
Christopher Baker <me@christopherbaker.net>
2018-03-05 20:19:24 -0600

    Add tagged LeNet.

--- libs/ofxDlib/include/ofx/Dlib/Network/LeNet.h
+++ libs/ofxDlib/include/ofx/Dlib/Network/LeNet.h
@@ -46,8 +46,34 @@ using LeNet = dlib::loss_multiclass_log<
                                         dlib::max_pool<2,2,2,2,dlib::relu<dlib::con<6,5,5,1,1,
                                         dlib::input<dlib::matrix<unsigned char>>
                                         >>>>>>>>>>>>;
+ 
+template <typename SUBNET> using tlnet0  = dlib::add_tag_layer< 1100 + 0, SUBNET>;
+template <typename SUBNET> using tlnet1  = dlib::add_tag_layer< 1100 + 1, SUBNET>;
+template <typename SUBNET> using tlnet2  = dlib::add_tag_layer< 1100 + 2, SUBNET>;
+template <typename SUBNET> using tlnet3  = dlib::add_tag_layer< 1100 + 3, SUBNET>;
+template <typename SUBNET> using tlnet4  = dlib::add_tag_layer< 1100 + 4, SUBNET>;
+template <typename SUBNET> using tlnet5  = dlib::add_tag_layer< 1100 + 5, SUBNET>;
+template <typename SUBNET> using tlnet6  = dlib::add_tag_layer< 1100 + 6, SUBNET>;
+template <typename SUBNET> using tlnet7  = dlib::add_tag_layer< 1100 + 7, SUBNET>;
+template <typename SUBNET> using tlnet8  = dlib::add_tag_layer< 1100 + 8, SUBNET>;
+template <typename SUBNET> using tlnet9  = dlib::add_tag_layer< 1100 + 9, SUBNET>;
+template <typename SUBNET> using tlnet10  = dlib::add_tag_layer< 1100 + 10, SUBNET>;
+
+using TaggedLeNet = dlib::loss_multiclass_log<
+                    tlnet0<dlib::fc<10,
+                    tlnet1<dlib::relu<
+                    tlnet2<dlib::fc<84,
+                    tlnet3<dlib::relu<
+                    tlnet4<dlib::fc<120,
+                    tlnet5<dlib::max_pool<2,2,2,2,
+                    tlnet6<dlib::relu<
+                    tlnet7<dlib::con<16,5,5,1,1,
+                    tlnet8<dlib::max_pool<2,2,2,2,
+                    tlnet9<dlib::relu<
+                    tlnet10<dlib::con<6,5,5,1,1,
+                    dlib::input<dlib::matrix<unsigned char>
+                    >>>>>>>>>>>>>>>>>>>>>>>>;
 
 
-    
     
 } } } // namespace ofx::Dlib::Network
