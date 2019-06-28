cebc50c1cf4fdb827a9218d809b87aa1956eb74b
Christopher Baker <info@christopherbaker.net>
2019-06-28 00:46:57 -0600

    Initial test support for armv6l (rpi).

--- addon_config.mk
+++ addon_config.mk
@@ -44,6 +44,19 @@ osx:
 	# ADDON_DEFINES  += MKL_ILP64
 	# ADDON_CPPFLAGS += -m64
 
+linuxarmv6l:
+	ADDON_LIBS = libs/dlib/lib/linuxarmv6l/libdlib.a
+	ADDON_PKG_CONFIG_LIBRARIES = libpng libjpeg
+	ADDON_LDFLAGS += -lgif
+
+	# For processors that support neon SIMD instructions.
+	# dlib on the Rapberry Pi can benefit further from profiling.
+	# See the discussion here: https://github.com/davisking/dlib/issues/557
+	ADDON_CPPFLAGS += -O3 -mfpu=neon
+
+	# If dlib is compiled with libblas/liblapack support, you may need to include these.
+	ADDON_PKG_CONFIG_LIBRARIES += blas lapack
+
 linux64:
 	ADDON_LIBS = libs/dlib/lib/linux64/libdlib.a
 	ADDON_PKG_CONFIG_LIBRARIES = libpng libjpeg
