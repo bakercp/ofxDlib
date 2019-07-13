2f496cba54fdc6ce22de8659a37ce7e9c2d43f9c
Christopher Baker <me@christopherbaker.net>
2019-07-13 15:45:57 -0600

    Add curl if it isn't already installed.

--- scripts/linux/install.sh
+++ scripts/linux/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-sudo apt-get -qq install -y cmake libblas-dev liblapack-dev libpng-dev libcairo2-dev libjpeg-dev libgif-dev > /dev/null
+sudo apt-get -qq install -y curl cmake libblas-dev liblapack-dev libpng-dev libcairo2-dev libjpeg-dev libgif-dev > /dev/null
