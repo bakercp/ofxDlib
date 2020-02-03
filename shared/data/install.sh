30d47fa6885807e8c2e75ca49cba7d73a8f7419c
Hiroshi Matoba <matobahiroshi@gmail.com>
2020-02-03 21:21:44 +0100

    install script

--- shared/data/install.sh
+++ shared/data/install.sh
@@ -1,45 +1,63 @@
 #!/usr/bin/env bash
 set -e
 
-THIS_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+if [ -z "$1" ]; then
+    echo "Usage: install_data_for_examples <path_to_addon>"
+else
+    # Form the shared data path.
+    addon_shared_data_path=${1}/shared/data
 
-/usr/bin/env bash ${THIS_PATH}/mnist.sh
-/usr/bin/env bash ${THIS_PATH}/dlib.sh
-/usr/bin/env bash ${THIS_PATH}/media.sh
+    dirs=${1}/example*
+    for dir in $dirs
+    do
+    echo "Process ${dir}"
 
-# function copy_shared_data_for_examples()
-# {
-#   if [ -z "$1" ]; then
-#     echo "Usage: install_data_for_examples <path_to_addon>"
-#     return 1
-#   fi
-#
-#   # Form the shared data path.
-#   addon_shared_data_path=${1}/shared/data
-#
-#   for required_data in ${1}/example*/bin/data/REQUIRED_DATA.txt
-#   do
-#     # For the example data path.
-#     example_data_path=$(dirname ${required_data})
-#
-#     # The || [ -n "$line" ]; is to help when the last line isn't a new line char.
-#     while read line || [ -n "$line" ];
-#     do
-#       # Make sure the data doesn't start with a comment hash #
-#       # Make sure that it isn't am empty line.
-#       if [ "${line:0:1}" != "#"  ] && [ -n "${line// }" ]; then
-#         # Turn the line into an array (space delimited).
-#         tokens=( "$line" )
-#         # Get the first token -- the source location.
-#         data_source=${tokens[0]}
-#
-#         if [ -e ${addon_shared_data_path}/${data_source} ]; then
-#           rsync -Pqar ${addon_shared_data_path}/${data_source} ${example_data_path}
-#         else
-#           echo "${addon_shared_data_path}/${data_source} does not exist. Did you install the data?"
-#         fi
-#       fi
-#     done < $required_data
-#   done
-#   return 0
-# }
+    required_data=${dir}/bin/data/data.txt
+
+    if [ -f "$required_data" ]; then
+        #echo ${required_data}
+        # For the example data path.
+        example_data_path=$(dirname ${required_data})
+
+        # The || [ -n "$line" ]; is to help when the last line isn't a new line char.
+        while read line || [ -n "$line" ];
+        do
+        # Make sure the data doesn't start with a comment hash #
+        # Make sure that it isn't am empty line.
+        if [ "${line:0:1}" != "#"  ] && [ -n "${line// }" ]; then
+            # Turn the line into an array (space delimited).
+            tokens=($line)
+            # Get the first token -- the source location.
+            data_source="${tokens[0]}"
+            data_destination="${tokens[1]}"
+
+            # remove trailing whitespace
+            #data_source="$(echo -e "${data_source}" | sed -e 's/[[:space:]]*$//')"
+            data_destination="$(echo -e "${data_destination}" | sed -e 's/[[:space:]]*$//')"
+
+            src="${addon_shared_data_path}/${data_source}"
+            dst="${example_data_path}/${data_destination}"
+
+            if [ "${data_destination}" == "" ]; then
+                # copy folder contents into /data folder
+                cp -r "${src}/." "${example_data_path}/"
+            else
+                if [ -d "${src}" ]; then
+                    echo "  copy directory ${data_source}"
+                    cp -r $src $dst
+                elif [ -f "${src}" ]; then
+                    echo "  copy file ${data_source}"
+                    cp $src $dst
+                else
+                    echo "  ${addon_shared_data_path}/${data_source} does not exist. Did you install the data?"
+                fi
+            fi
+        fi
+        done < $required_data
+    else
+        echo "  data.txt does not exist, skip copy"
+    fi
+
+        echo ""
+    done
+fi
